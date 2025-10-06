f6dfb56d1fa9c61984dae78aa189eb7c2b62fdbb
jakepowneyrhino <jake.powney@rhinogroup.co.uk>
2025-10-06 16:19:38 +0100

    LocalSettings skin

--- LocalSettings.php
+++ LocalSettings.php
@@ -124,7 +124,7 @@ $wgDiff3 = "/usr/bin/diff3";
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "vector";
+$wgDefaultSkin = "Vector";
 
 # End of automatically generated settings.
 # Add more configuration options below.
